8a02cd8c493845d4c378c77bb0b64351a00bc5c6
Jean-Kevin KPADEY <jeankevin.kpadey@gmail.com>
2025-02-11 22:40:09 +0100

    refactor: remove the pgbackrest_spool_directory
    The spool-path is computed from the options

--- filter_plugins/pgbackrest.py
+++ filter_plugins/pgbackrest.py
@@ -8,9 +8,18 @@ def repositories_path(repositories_list):
             in repository['options'] if option['name'] == 'path']
 
 
+def spool_path(options_list):
+    """
+    Returns the spool path from the options list.
+    """
+    return [option['value'] for option
+            in options_list if option['name'] == 'spool-path']
+
+
 class FilterModule(object):
 
     def filters(self):
         return {
-            'repositories_path': repositories_path
+            'repositories_path': repositories_path,
+            'spool_path': spool_path
         }
